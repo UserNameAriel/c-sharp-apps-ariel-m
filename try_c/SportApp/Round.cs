31e50eb1d14b068ce30abd0aaddd7f312140082e
ariel mizrahu <arimiz5928@gmail.com>
2024-06-07 10:17:32 +0300

    game class

---
+++ try_c/SportApp/Round.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_sharp_App_ArielM.SportApp
+{
+    internal class Round
+    {
+        private Game[] gamse;
+    }
+}
